8dc1cd34a07138669b44e68e727cc9302a2b9413
Dan Day <danday@gmail.com>
2021-03-21 19:20:29 -0500

    Rename dictionary folder

--- hangman/mix.exs
+++ hangman/mix.exs
@@ -22,7 +22,7 @@ defmodule Hangman.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dictionary, [path: "./dictionary"]}
+      {:dictionary, [path: "./dict"]}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
